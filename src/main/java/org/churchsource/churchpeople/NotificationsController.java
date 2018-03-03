8a9cae9b0439854623e8ca22110392de8119d93f
rowan84 <rowan.pillay@jembi.org>
2018-03-03 15:28:51 +0200

    Add notification endpoint

---
+++ src/main/java/org/churchsource/churchpeople/NotificationsController.java
@@ -0,0 +1,29 @@
+package org.churchsource.churchpeople;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("notifications")
+public class NotificationsController {
+
+  
+  @PersistenceContext
+  private EntityManager entityManager;
+  
+  @GetMapping("/{id}")
+  public String index() {
+      return "Greetings from Spring!";
+  }
+  
+  @RequestMapping(method = RequestMethod.POST)
+  public String printNotification(@RequestBody String message) {
+      return "Message: " + message;
+  }
+}
