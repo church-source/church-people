21ccff9c305eda86610cc66942c90da165f1cd7f
rowan84 <rowan.pillay@jembi.org>
2018-03-04 16:10:25 +0200

    Add request loggin

--- src/main/java/org/churchsource/churchpeople/ChurchPeopleApplication.java
+++ src/main/java/org/churchsource/churchpeople/ChurchPeopleApplication.java
@@ -3,11 +3,24 @@ package org.churchsource.churchpeople;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@Configuration
 public class ChurchPeopleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChurchPeopleApplication.class, args);
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    return loggingFilter;
 	}
 }
