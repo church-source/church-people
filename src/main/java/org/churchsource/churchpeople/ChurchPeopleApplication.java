edcdbb10f0f7470d54e4149c42c544d70db1cafe
rowanpi <rowan.pillay@jembi.org>
2019-12-19 14:25:47 +0200

    Add very basic security. Note this is not secure at all and is just a temporary means, until proper authentication is setup.

--- src/main/java/org/churchsource/churchpeople/ChurchPeopleApplication.java
+++ src/main/java/org/churchsource/churchpeople/ChurchPeopleApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class })
 @Configuration
 public class ChurchPeopleApplication {
 
@@ -22,5 +25,15 @@ public class ChurchPeopleApplication {
 	    loggingFilter.setIncludeQueryString(true);
 	    loggingFilter.setIncludePayload(true);
 	    return loggingFilter;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
---
+++ src/main/java/org/churchsource/churchpeople/security/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package org.churchsource.churchpeople.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .csrf().disable()
+      .cors().disable()
+      .authorizeRequests()
+      .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()//allow CORS option calls
+      .antMatchers("/resources/**").permitAll()
+      .anyRequest().authenticated()
+      .and()
+        .httpBasic()
+      .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+        .requestCache().disable();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+      web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth)
+          throws Exception
+  {
+    auth.
+    auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("password")
+            .roles("USER");
+  }
+}
