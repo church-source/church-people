883cdef6515fad7911545b2d572e100bd4216ccb
rowanpi <rowan.pillay@jembi.org>
2020-02-18 16:45:46 +0200

    Allow all cross-origins

--- src/main/java/org/churchsource/churchpeople/security/BasicAuthenticationController.java
+++ src/main/java/org/churchsource/churchpeople/security/BasicAuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
-@CrossOrigin(origins={ "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin
 @RestController
 public class BasicAuthenticationController {
 
